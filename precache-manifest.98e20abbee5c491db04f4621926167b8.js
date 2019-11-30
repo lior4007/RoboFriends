863b904e0ff8097c3d3f45e5a29a3249cdd4698d
Lior <lior4007@gmail.com>
2019-12-01 00:53:07 +0200

    Updates

---
+++ precache-manifest.98e20abbee5c491db04f4621926167b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07eec63bbabb7b0d22d55ba908dde58",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "53ef5564d911eed9bbb6",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "52da319cb467cf983b46",
+    "url": "/RoboFriends/static/css/main.f86bc083.chunk.css"
+  },
+  {
+    "revision": "53ef5564d911eed9bbb6",
+    "url": "/RoboFriends/static/js/2.7181e3f3.chunk.js"
+  },
+  {
+    "revision": "52da319cb467cf983b46",
+    "url": "/RoboFriends/static/js/main.4e89df32.chunk.js"
+  },
+  {
+    "revision": "e247e82dac7f87e47e91",
+    "url": "/RoboFriends/static/js/runtime-main.f85fd60c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
